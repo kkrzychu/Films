4e5cdde60cd07bfc8defbc9630da39ffeea4fc2a
Krzychu <krzymich1997@gmail.com>
2020-04-06 00:26:36 +0200

    Second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { FilmsComponent } from './pages/films/films.component';
+import { SerialsComponent } from './pages/serials/serials.component';
+import { RankingComponent } from './pages/ranking/ranking.component';
+import { MyProfileComponent } from './pages/my-profile/my-profile.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'filmy', component: FilmsComponent },
+  { path: 'seriale', component: SerialsComponent },
+  { path: 'ranking', component: RankingComponent },
+  { path: 'mojProfil', component: MyProfileComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './navigation/footer/footer.component';
 import { NavbarComponent } from './navigation/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FilmsComponent } from './pages/films/films.component';
+import { SerialsComponent } from './pages/serials/serials.component';
+import { RankingComponent } from './pages/ranking/ranking.component';
+import { MyProfileComponent } from './pages/my-profile/my-profile.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    FilmsComponent,
+    SerialsComponent,
+    RankingComponent,
+    MyProfileComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
